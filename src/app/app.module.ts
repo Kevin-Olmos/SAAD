8e91fcb9c5a8ed626abe4f33e5e5d5f74af9c7ce
EstebanAdame <esteban.adame@uabc.edu.mx>
2022-06-09 10:20:55 -0700

    inicio y dashboard

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,14 @@ import { RouterModule } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AuthModule } from './auth/auth/auth.module';
+import { InicioComponent } from './auth/components/inicio/inicio.component';
+import { DashboardComponent } from './auth/components/dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    InicioComponent,
+    DashboardComponent,
   ],
   imports: [
     BrowserModule,
